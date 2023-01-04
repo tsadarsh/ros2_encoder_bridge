9376086e31553ea682e19d74646c66520b34cdd4
tsadarshVSC <tsadarsh0707@gmail.com>
2023-01-04 18:41:32 +0530

    Configured `setup.py`

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'ros2_encoder_bridge'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='tsadarsh',
     maintainer_email='tsadarsh0707@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Publishes encoder ticks form Arduino Nano using Serial Protocol',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'encoder_publisher = ros2_encoder_bridge.encoder_publisher:main',
         ],
     },
 )
